51523bea96da60ae6be8c56b1dac8b93151dd04d
Alfred Hans <112923618+Zwelious@users.noreply.github.com>
2024-05-13 16:31:59 +0700

    template login for joy
    joyjasio

---
+++ resources/views/login.blade.php
@@ -0,0 +1,11 @@
+@extends("main")
+
+@section("body")
+
+<div class="container-fluid py-5 mb-5">
+    <div class="container py-5">
+        <h2 style="color:black">Login</h2>
+    </div>
+</div>
+
+@endsection
