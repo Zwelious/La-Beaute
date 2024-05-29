4151b9caa133948bf98b6f838cdff7c9b1f28078
Alfred Hans <112923618+Zwelious@users.noreply.github.com>
2024-05-29 15:01:21 +0700

    shop

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ShopController extends Controller
 {
     public function Shop()
     {
-        return view('shop');
+        $dataProducts = DB::table('detail_produk')
+        ->select('ID_PROD', 'NAMA_PROD', 'SHADE', 'DESKRIPSI', 'HARGA', 'DISKON', 'KATEGORI', 'STOCK', 'FOTO_PROD')
+        ->paginate(9);
+        return view('shop', compact('dataProducts'));
     }
 }
